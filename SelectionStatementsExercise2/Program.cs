6c53506eaf732ecd146c959892e172e100844bcf
Will Royal <william.j.royal@gmail.com>
2023-08-08 21:59:41 -0700

    initial commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -5,33 +5,35 @@
         static void Main(string[] args)
         {
             Console.WriteLine("What is your favorite subject in school?");
-            var favSub = Console.ReadLine();
+            string favSub = Console.ReadLine();
 
-            if (favSub == "Math")
+            switch (favSub.ToLower())
             {
-                Console.WriteLine($"{favSub} is all about arithmetic.");
-            }
-            else if (favSub == "English")
-            {
-                Console.WriteLine($"{favSub} is so important to a student's academic journey.");
-            }
-            else if (favSub == "Biology")
-            {
-                Console.WriteLine($"Many students have to dissect an animal in {favSub} class.");
-            }
-            else if (favSub == "Chemistry")
-            {
-                Console.WriteLine($"If you are taking {favSub}, be sure to wear proper PPE!");
+                case "math":
+                    Console.WriteLine("I hate math");
+                    break;
 
+                case "science":
+                    Console.WriteLine("I love science");
+                    break;
+
+                case "pe":
+                    Console.WriteLine("PE is the best");
+                    break;
+
+                case "history":
+                    Console.WriteLine("History is fun");
+                    break;
+
+                case "english":
+                    Console.WriteLine("English is boring");
+                    break;
+
+                default:
+                    Console.WriteLine("I don't know that subject");
+                    break;
             }
-            else if (favSub == "P.E.")
-            {
-                Console.WriteLine($"Make sure to stretch before and after {favSub}.");
-            }
-            else
-            {
-                Console.WriteLine($"{favSub} is an awesome class. Make sure to study hard!");
-            }
+            
         }
     }
 }
